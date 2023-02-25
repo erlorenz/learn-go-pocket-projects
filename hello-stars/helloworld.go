110335335373347be75d2110e74e5472345b9050
Erik Lorenz <erik@eriklorenz.dev>
2023-02-25 01:39:25 -0600

    implemented as map

--- hello-stars/helloworld.go
+++ hello-stars/helloworld.go
@@ -3,11 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	greeting := greet()
-
+	greeting := greet("en")
 	fmt.Println(greeting)
 }
 
-func greet() string {
-	return "Hello world"
+// language representst the language's code
+type language string
+
+// Phrasebook holds greeting for each language
+var phrasebook = map[language]string{
+	"en": "Hello world",
+	"fr": "Bonjour le monde",
+}
+
+func greet(l language) string {
+	greeting, ok := phrasebook[l]
+	if !ok {
+		return fmt.Sprintf("unsupported language: %q", l)
+	}
+
+	return greeting
 }
--- hello-stars/helloworld_internal_test.go
+++ hello-stars/helloworld_internal_test.go
@@ -9,12 +9,39 @@ func ExampleMain() {
 }
 
 func TestGreet(t *testing.T) {
-	expectedGreeting := "Hello world"
 
-	greeting := greet()
+	type testCase struct {
+		lang             language
+		expectedGreeting string
+	}
 
-	if greeting != expectedGreeting {
-		// mark this test as failed
-		t.Errorf("expected: %q, got: %q", expectedGreeting, greeting)
+	var tests = map[string]testCase{
+		"English": {
+			lang:             "en",
+			expectedGreeting: "Hello world",
+		},
+		"French": {
+			lang:             "fr",
+			expectedGreeting: "Bonjour le monde",
+		},
+		"Akkadian": {
+			lang:             "akk",
+			expectedGreeting: `unsupported language: "akk"`,
+		},
+		"Unsupported": {
+			lang:             "",
+			expectedGreeting: `unsupported language: ""`,
+		},
+	}
+
+	// range over all the tests
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			greeting := greet(tc.lang)
+
+			if greeting != tc.expectedGreeting {
+				t.Errorf("expected: %q, got: %q", tc.expectedGreeting, greeting)
+			}
+		})
 	}
 }
